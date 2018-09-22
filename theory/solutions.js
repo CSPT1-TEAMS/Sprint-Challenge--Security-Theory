b90cf762ce61791695c3ad5ce22fa389a17f6d51
Andrew Jarrett <ahrjarrett@gmail.com>
2018-09-22 11:21:15 -0500

    finish regex problems in solution.js

--- theory/solutions.js
+++ theory/solutions.js
@@ -22,7 +22,14 @@ console.log(firstRegex.test('antelopes rock out')) // => true
  * but not:
 
  *    boat
+ */
 
+let secondRegex = /[gm]oat/
+console.log(secondRegex.test('goat')) // => true
+console.log(secondRegex.test('moat')) // => true
+console.log(secondRegex.test('boat')) // => false
+
+/*
  * Regex that matches dates in YYYY-MM-DD format. 
  * (Year can be 1-4 digits, and month and day can 
  * each be 1-2 digits). This does not need to verify 
@@ -36,3 +43,12 @@ console.log(firstRegex.test('antelopes rock out')) // => true
   
   812-2-10
  */
+
+console.log('\n')
+
+let thirdRegex = /\d{1,4}-\d{1,2}-\d{1,2}/
+console.log(thirdRegex.test('2000-10-12'))
+console.log(thirdRegex.test('1999-1-20'))
+console.log(thirdRegex.test('1999-01-20'))
+console.log(thirdRegex.test('812-2-10'))
+
